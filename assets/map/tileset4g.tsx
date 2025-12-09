cf74aac958821acf172d8a69ff5cddb883eed372
Maelle <maelle.debard@eleve.isep.fr>
2025-12-09 20:20:56 +0100

    c'est bon ?

--- assets/map/tileset4g.tsx
+++ assets/map/tileset4g.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="tileset4g" tilewidth="16" tileheight="16" tilecount="21000" columns="140">
+<tileset version="1.10" tiledversion="1.11.2" name="tileset4g" tilewidth="16" tileheight="16" tilecount="21000" columns="140">
  <image source="tileset4g.png" width="2240" height="2400"/>
  <tile id="3680">
   <properties>
